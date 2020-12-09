f535f2a8c107b568f499f58ac771130899fd46ff
Ayman Mir <aymanmir@gmail.com>
2020-12-09 12:57:19 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -14,7 +14,7 @@ class paper
 		this.y=y;
 		this.r=r
 		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
-		this.image=loadImage("sprites/paper.png");
+		this.image=loadImage("paper.png");
 		World.add(world, this.body);
 
 	}
